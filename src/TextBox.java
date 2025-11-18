15f840603043ce0e22f0cac9f08ed2b7a2a430fc
Marques <gaston.marques@gmail.com>
2025-11-18 12:42:18 -0500

    Part 2 - Inheritance

--- src/TextBox.java
+++ src/TextBox.java
@@ -1,5 +1,6 @@
-public class TextBox {
-    public String text = ""; // Field
+// the extends UIControl extends all methods into Textbox (checkout main)
+public class TextBox extends UIControl {
+    private String text = ""; // Field
 
     public void setText(String text) {
         this.text = text;
