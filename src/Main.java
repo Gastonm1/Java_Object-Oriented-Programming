095e12049b1de39a46a9230032440530614b041c
Marques <gaston.marques@gmail.com>
2025-11-19 10:57:41 -0500

    Part 2 - Section: Inheritance - bring back checkbox

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        UIControl[] controls = { new TextBox() };
+        UIControl[] controls = { new TextBox(), new Checkbox() };
         for (var control : controls) {
             control.render();
         }
