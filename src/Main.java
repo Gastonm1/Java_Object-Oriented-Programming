47475905070197c9be41b11107759b58288a2b64
Marques <gaston.marques@gmail.com>
2025-11-19 14:28:12 -0500

    Part 2 - Section: Interfaces - Method Injection

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         var calculator = new TaxCalculator2025(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
-
-        report.setCalculator(new TaxCalculator2026());
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
+        report.show(new TaxCalculator2026());
     }
 }
