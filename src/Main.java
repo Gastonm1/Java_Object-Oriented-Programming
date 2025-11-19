d5aed70ea502c48eb6fdf1c42d18e29c02d855ab
Marques <gaston.marques@gmail.com>
2025-11-19 14:11:52 -0500

    Part 2 - Section: Interfaces - Constructor Injection

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        var calculator = new TaxCalculator2025(100_000);
+        var report = new TaxReport(calculator);
     }
 }
--- src/TaxReport.java
+++ src/TaxReport.java
@@ -1,8 +1,8 @@
 public class TaxReport {
-private TaxCalculator2025 calculator;
+private TaxCalculator calculator;
 
-public TaxReport() {
-    calculator = new TaxCalculator2025(100_000);
+public TaxReport(TaxCalculator calculator) {
+   this.calculator = calculator;
 }
 
 public void show(){
